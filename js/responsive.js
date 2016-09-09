885965f7d9b9de037593855360357b72603f5e29
Satyen Akolkar <s.akolkar3@gmail.com>
2016-09-08 20:37:11 -0600

    init repo

---
+++ js/responsive.js
@@ -0,0 +1,31 @@
+// Responsive Scrolling Page based on tutorial by Ali Jafarian
+// http://alijafarian.com/responsive-page-scrolling-with-jquery-and-bootstrap/
+$(document).ready(function() {
+  $('.scroll-link').on('click', function(event) {
+    event.preventDefault();
+    var sectionID = $(this).attr("data-id");
+    scrollToID('#' + sectionID, 750);
+  })
+
+  $('scroll-top').on('click', function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop:0}, 'slow');
+  });
+
+  setSectionHeights();
+});
+
+function scrollToID(id, speed) {
+  var target = $(id).offset().top;
+  $('html, body').animate({scrollTop:target}, speed);
+}
+
+function setSectionHeights() {
+  var windowH = $(window).height();
+
+  $('.page-section').each(function (i) {
+    if ($(this).height() < windowH) {
+      $(this).css({height: windowH});
+    }
+  });
+}
